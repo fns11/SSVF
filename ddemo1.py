8469c1b8167cfc2db3cb3febcb63b9ff938c2f3c
Code_House <105477971+fns11@users.noreply.github.com>
2022-06-12 22:25:40 -0400

    Update ddemo1.py

--- ddemo1.py
+++ ddemo1.py
@@ -3,7 +3,7 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 from PIL import Image
 
-uploaded_file = st.file_uploader('CIntake Packet1 SSVF', type="pdf")
+uploaded_file = st.file_uploader('Intake Packet1 SSVF.pdf', type="pdf")
 if uploaded_file is not None:
     df = extract_data(uploaded_file)
 
