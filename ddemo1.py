280422da00a2467f158787ebccc225e8ce27cb7a
Code_House <105477971+fns11@users.noreply.github.com>
2022-06-16 01:42:44 -0400

    Update ddemo1.py

--- ddemo1.py
+++ ddemo1.py
@@ -116,15 +116,12 @@ def main():
 if __name__ == '__main__':
 	main()   
 # ---- WHAT I DO ----
-
-	with st.container():
-		
-		
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
 	
-st.write("---")
-left_column, right_column = st.columns(2)
-with left_column:
-            st.header("What we do to get you the assistance you need")
+        st.header("What we do to get you the assistance you need")
 	
     	st.write("##")
     	st.write("[YouTube Channel >](https://youtu.be/zjTE48Ab9J0)")
