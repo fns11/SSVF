2a48ba9544af23c8db350d45e56858254ec8fcdc
Code_House <105477971+fns11@users.noreply.github.com>
2022-06-20 12:33:36 -0400

    Update ddemo1.py

--- ddemo1.py
+++ ddemo1.py
@@ -5,6 +5,19 @@ from streamlit_lottie import st_lottie
 from PIL import Image
 import pandas as pd 
 import base64
+from streamlit_option_menu import option_menu
+
+with st.sidebar:
+    choose = option_menu("Main Menu", ["About", "Projects", "Blog","Apps", "Contact"],
+                         icons=['house', 'bar-chart-line','file-slides','app-indicator','person lines fill'],
+                         menu_icon="list", default_index=0,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#24A608"},
+    }
+    )
 
 # Function to Display PDF in Streamlit
 def st_display_pdf('SSVF HP Screener v6 9.2021.pdf'):
