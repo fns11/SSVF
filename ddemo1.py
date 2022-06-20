3526ebf7f4fc39873e1edaa2af8959d8cc77005e
Code_House <105477971+fns11@users.noreply.github.com>
2022-06-20 12:50:34 -0400

    Update ddemo1.py

--- ddemo1.py
+++ ddemo1.py
@@ -21,8 +21,8 @@ with st.sidebar:
 
 # Function to Display PDF in Streamlit
 
-        st.write('')
-        def show_pdf(file_path):
+       st.write('')
+       def show_pdf(file_path):
             with open(file_path,"rb") as f:
                 base64_pdf = base64.b64encode(f.read()).decode('utf-8')
             pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
