f549aa8f44e7415ce3823de91f370f79c41ad44c
Code_House <105477971+fns11@users.noreply.github.com>
2022-06-21 14:47:39 -0400

    Update ddemo1.py

--- ddemo1.py
+++ ddemo1.py
@@ -28,6 +28,21 @@ st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
 
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+
+# Use local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style_css.txt")
+
 
 def csv_downloader(data):
 	csvfile = data.to_csv()
