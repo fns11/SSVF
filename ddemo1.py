315bd473e68ec2807ed52c5c4fc57b64d3be0456
Code_House <105477971+fns11@users.noreply.github.com>
2022-06-22 21:15:55 -0400

    Update ddemo1.py

--- ddemo1.py
+++ ddemo1.py
@@ -74,7 +74,7 @@ elif choose == "Blog":
             with col2:
                 st.button('Close PDF Tutorial',key='2')                   
             with col3:
-                with open("post1-compressed.pdf", "rb") as pdf_file:
+                with open('SSVF HP Screener v6 9.2021.pdf', "rb") as pdf_file:
                     PDFbyte = pdf_file.read()
                 st.download_button(label="Download PDF Tutorial", key='3',
                         data=PDFbyte,
